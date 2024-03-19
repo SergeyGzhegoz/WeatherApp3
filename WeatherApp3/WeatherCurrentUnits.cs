38d33933f6098d5985630607cdf244138d5ea509
SergeyGzhegoz <s.gz@yandex.ru>
2024-03-19 13:49:04 +0300

    Добавьте файлы проекта.

---
+++ WeatherApp3/WeatherCurrentUnits.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp3
+{
+    class WeatherCurrentUnits
+    {
+        public string Temperature_2m { get; set; }
+        public string Relative_humidity_2m { get; set; }
+    }
+}
